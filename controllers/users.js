a02278a9076820bccb83ef63c8f726477b952cd1
macnaer <trofimchuk.an@gmail.com>
2020-04-26 00:19:20 +0300

    Connected to atlas. Installed new modules

--- controllers/users.js
+++ controllers/users.js
@@ -4,10 +4,18 @@ module.exports = function (_) {
   return {
     SetRouting: function (router) {
       router.get("/", this.indexPage);
+      router.get("/register", this.registerPage);
+      router.get("/login", this.loginPage);
     },
 
     indexPage: function (req, res) {
       return res.render("index", { title: "Test | pass" });
+    },
+    registerPage: function (req, res) {
+      return res.render("register");
+    },
+    loginPage: function (req, res) {
+      return res.render("login");
     },
   };
 };
