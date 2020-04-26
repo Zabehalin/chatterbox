a232d224a3b27dc267d4e30b73ea3f9079a7347d
Greendiax <artlynx0@gmail.com>
2020-04-26 10:17:38 +0300

    Created template skeleton && installed new modules

--- controllers/users.js
+++ controllers/users.js
@@ -4,10 +4,22 @@ module.exports = function (_) {
   return {
     SetRouting: function (router) {
       router.get("/", this.indexPage);
+      router.get("/register", this.registerPage);
+      router.get("/login", this.loginPage);
+      router.get("/forgot", this.forgotPage);
     },
 
     indexPage: function (req, res) {
       return res.render("index", { title: "Test | pass" });
+    },
+    registerPage: function (req, res) {
+      return res.render("register");
+    },
+    loginPage: function (req, res) {
+      return res.render("login");
+    },
+    forgotPage: function (req, res) {
+      return res.render("forgot");
     },
   };
 };
