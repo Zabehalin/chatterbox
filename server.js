dbaa801161e5915c4b641dbeb55bc1c13dd865db
macnaer <trofimchuk.an@gmail.com>
2020-04-28 00:36:14 +0300

    Added passport-local strategy

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ container.resolve(function (users, _) {
   }
 
   function ConfigureExpress(app) {
+    require("./passport/passport-local");
     app.use(express.static("public"));
     app.use(cookieParser());
     app.set("view engine", "ejs");
