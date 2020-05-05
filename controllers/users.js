6aa82528efec5afccf719bd25da893c1b6ab1cb9
macnaer <trofimchuk.an@gmail.com>
2020-05-05 08:39:41 +0300

    Fixed issue with display error. Refactor main routes

--- controllers/users.js
+++ controllers/users.js
@@ -3,36 +3,42 @@
 module.exports = function (_, passport, User) {
   return {
     SetRouting: function (router) {
-      router.get("/", this.indexPage);
+      router.get("/", this.loginPage);
+      router.get("/home", this.indexPage);
 
       router.get("/register", this.registerPage);
       router.post("/register", User.RegisterValidation, this.postRegister);
-
-      router.get("/login", this.loginPage);
-      router.post("/login", this.postLogin);
+      router.post("/", this.postLogin);
     },
 
     indexPage: function (req, res) {
-      return res.render("index", { title: "Home page" });
+      return res.render("index", {
+        title: "Chatterbox | Main",
+      });
     },
     registerPage: function (req, res) {
       const errors = req.flash("error");
       return res.render("register", {
         title: "Chatterbox | Login",
         messages: errors,
-        hasErrors: errors.Length > 0,
+        hasErrors: errors.length > 0,
       });
     },
     loginPage: function (req, res) {
-      return res.render("login");
+      const errors = req.flash("error");
+      return res.render("login", {
+        title: "Chatterbox | Login",
+        messages: errors,
+        hasErrors: errors.length > 0,
+      });
     },
     postRegister: passport.authenticate("local.signup", {
-      successRedirect: "/",
+      successRedirect: "/home",
       failureRedirect: "/register",
       failureFlash: true,
     }),
     postLogin: passport.authenticate("local.login", {
-      successRedirect: "/",
+      successRedirect: "/home",
       failureRedirect: "/login",
       failureFlash: true,
     }),
